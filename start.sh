ed510079310aee5eeb3972cafa703d23ed3fb3e4
Cedric Beck <cedric.beck@web.de>
2025-04-08 17:02:48 +0200

    edited start script

--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+# --- whisper.cpp build ---
+if [ ! -d "whisper.cpp/build" ]; then
+  echo "Building whisper.cpp with Vulkan support..."
+  cd whisper.cpp || exit
+  cmake -B build -DGGML_VULKAN=1
+  cmake --build build -j --config Release
+  cd ..
+fi
 
-cd whisper.cpp
-cmake -B build -DGGML_VULKAN=1
-cmake --build build -j --config Release
+
+# --- Virtual environment setup ---
+if [ ! -d "venv" ]; then
+  echo "Creating virtual environment..."
+  python -m venv venv
+fi
+
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "Activating"
+  source venv/bin/activate
+  pip install -r requirements.txt
+fi
+
+python script.py
